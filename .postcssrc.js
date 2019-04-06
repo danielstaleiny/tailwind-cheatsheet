a98431cf2693105be0f751e98dd9af28d355408d
danielstaleiny <daniel.rafaj@tuta.io>
2019-04-06 17:09:51 +0200

    tailwindcss setup

---
+++ .postcssrc.js
@@ -0,0 +1,26 @@
+const tailwindcss = class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+    }
+}
+
+module.exports = {
+    plugins: [
+        require('postcss-easy-import'),
+        require('tailwindcss')('./src/pages/assets/css/tailwind.config.js'),
+        process.env.NODE_ENV === 'production'
+            ? require('postcss-purgecss')({
+                  content: ['./src/pages/**/*.html'],
+                  extractors: [
+                      {
+                          extractor: TailwindExtractor,
+                          extensions: ['html', 'js']
+                      }
+                  ]
+              })
+            : function() {
+                  return []
+              },
+        require('autoprefixer')
+    ]
+}
