c00a17041069ac9efbf8b7216b6ecabfb400ef86
danielstaleiny <daniel.rafaj@tuta.io>
2019-04-07 13:24:12 +0200

    bug fix for purgecss, elimination of the css

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,4 +1,4 @@
-const tailwindcss = class TailwindExtractor {
+class TailwindExtractor {
     static extract(content) {
         return content.match(/[A-Za-z0-9-_:\/]+/g) || []
     }
@@ -8,19 +8,19 @@ module.exports = {
     plugins: [
         require('postcss-easy-import'),
         require('tailwindcss')('./src/pages/assets/css/tailwind.config.js'),
-        // process.env.NODE_ENV === 'production'
-        //     ? require('postcss-purgecss')({
-        //           content: ['./src/pages/**/*.html'],
-        //           extractors: [
-        //               {
-        //                   extractor: TailwindExtractor,
-        //                   extensions: ['html', 'js']
-        //               }
-        //           ]
-        //       })
-        //     : function() {
-        //           return []
-        //       },
+        process.env.NODE_ENV === 'production'
+            ? require('@fullhuman/postcss-purgecss')({
+                  content: ['./.tmp/parcel/**/*.html'],
+                  extractors: [
+                      {
+                          extractor: TailwindExtractor,
+                          extensions: ['html', 'js']
+                      }
+                  ]
+              })
+            : function() {
+                  return []
+              },
         require('autoprefixer')
     ]
 }
