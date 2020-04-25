2c41787f46d0a5995fe872efcf7a364e8ce8bcbd
Daniel Rafaj <daniel.rafaj@tuta.io>
2020-04-25 15:03:29 +0200

    in dev run 11ty and parcel separatly

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,22 +1,16 @@
-// console.log('.postcssrc.js: ', process.env.NODE_ENV);
+console.log('.postcssrc', process.env.NODE_ENV)
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['.tmp/parcel/**/*.html'],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:\.\/]+/g) || [],
+})
+
 module.exports = {
-    plugins: [
-        require('postcss-easy-import'),
-        require('tailwindcss')('./pages/assets/css/tailwind.config.js'),
-        process.env.NODE_ENV === 'production'
-            ? require('@fullhuman/postcss-purgecss')({
-                  content: ['./.tmp/parcel/**/*.html'],
-                  extractors: [
-                      {
-                          extractor: (content) =>
-                              content.match(/[A-Za-z0-9-_:\.\/]+/g) || [],
-                          extensions: ['html', 'js'],
-                      },
-                  ],
-              })
-            : function () {
-                  return []
-              },
-        require('autoprefixer'),
-    ],
+  plugins: [
+    require('postcss-easy-import'),
+    require('tailwindcss')('./pages/assets/css/tailwind.config.js'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 }
